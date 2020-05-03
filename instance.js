c21253ea7c01746d95bc54358683dd573d5cd83e
Alex Pogue <alex@epogue.com>
2020-05-03 13:25:29 -0500

    Initial commit

---
+++ instance.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+// creating an axios instance to our habit API baseURL. This instance will be utilized by API requests to the habit API.
+export default axios.create({
+  baseURL: "http://localhost:5000/",
+
+});
