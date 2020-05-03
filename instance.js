7f29940c8d50dbc07b8b4c006add8f8b167ac9f9
Alex Pogue <alex@epogue.com>
2020-05-03 14:05:20 -0500

    Add ip

--- instance.js
+++ instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // creating an axios instance to our habit API baseURL. This instance will be utilized by API requests to the habit API.
 export default axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "http://192.168.0.9:5000/",
 
 });
