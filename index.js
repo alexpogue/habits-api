b3520df20a602009020b7a91c2eaf466d8b7a733
Alex Pogue <alex@epogue.com>
2020-05-07 21:52:36 -0500

    Add class

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 import habit from "./instance";
 
-// Gets all goals from the goal api.
-export const getAllHabitGoals = async () => {
-  const result = await habit("goal/");
-  return result.data.data;
-};
+export default class HabitApi {
+  // Gets all goals from the goal api.
+  async function getAllHabitGoals() {
+    const result = await habit("goal/");
+    return result.data.data;
+  };
 
-// Gets a goal with matching goalId if invoked with a goalId.
-export const getHabitGoal = async goalId => {
-  const result = await habit("goal/" + goalId);
-  return result.data.data;
-};
+  // Gets a goal with matching goalId if invoked with a goalId.
+  async function getHabitGoal() {
+    const result = await habit("goal/" + goalId);
+    return result.data.data;
+  };
 
-// Posts a single goal to the goalAPI, returns with the new goal array.
-export const postHabitGoal = async data => {
-  await habit.post("goal/", { name: data });
-  const newGoalArray = await habit("goal/");
-  return newGoalArray.data.data;
-};
+  // Posts a single goal to the goalAPI, returns with the new goal array.
+  async function postHabitGoal() {
+    await habit.post("goal/", { name: data });
+    const newGoalArray = await habit("goal/");
+    return newGoalArray.data.data;
+  };
+}
