67a07e0cd55116e563bbafb14626ca52521f6a46
Alex Pogue <alex@epogue.com>
2020-05-07 22:48:02 -0500

    Take baseUri as parameter

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-import habit from "./instance";
+import axios from "axios";
 
 export default class HabitApi {
+  // baseUri should be something like `http://192.168.0.1:5000/`
+  constructor(baseUri) {
+    this.apiCaller = axios.create({
+      baseURL: baseUri,
+    });
+  };
+
   // Gets all goals from the goal api.
-  static async getAllHabitGoals() {
-    const result = await habit("goal/");
+  async getAllHabitGoals() {
+    const result = await this.apiCaller("goal/");
     return result.data.data;
   };
 
   // Gets a goal with matching goalId if invoked with a goalId.
-  static async getHabitGoal() {
-    const result = await habit("goal/" + goalId);
+  async getHabitGoal() {
+    const result = await this.apiCaller("goal/" + goalId);
     return result.data.data;
   };
 
   // Posts a single goal to the goalAPI, returns with the new goal array.
-  static async postHabitGoal() {
-    await habit.post("goal/", { name: data });
-    const newGoalArray = await habit("goal/");
+  async postHabitGoal() {
+    await this.apiCaller.post("goal/", { name: data });
+    const newGoalArray = await this.apiCaller("goal/");
     return newGoalArray.data.data;
   };
 }
--- instance.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-// creating an axios instance to our habit API baseURL. This instance will be utilized by API requests to the habit API.
-export default axios.create({
-  baseURL: "http://192.168.0.9:5000/",
-
-});
