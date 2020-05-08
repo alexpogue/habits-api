0ac7f5b89e1714eff9b7f726a153ac229de34ef3
Alex Pogue <alex@epogue.com>
2020-05-07 22:24:28 -0500

    Make functions static

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ import habit from "./instance";
 
 export default class HabitApi {
   // Gets all goals from the goal api.
-  async function getAllHabitGoals() {
+  static async getAllHabitGoals() {
     const result = await habit("goal/");
     return result.data.data;
   };
 
   // Gets a goal with matching goalId if invoked with a goalId.
-  async function getHabitGoal() {
+  static async getHabitGoal() {
     const result = await habit("goal/" + goalId);
     return result.data.data;
   };
 
   // Posts a single goal to the goalAPI, returns with the new goal array.
-  async function postHabitGoal() {
+  static async postHabitGoal() {
     await habit.post("goal/", { name: data });
     const newGoalArray = await habit("goal/");
     return newGoalArray.data.data;
